61fc546df058d5c5728a1cfa4745abf5b1f69a56
Rafael Carneiro <rafaelmdcarneiro@gmail.com>
2026-02-18 01:13:04 -0300

    Create vdp.cpp
    Initial commit.

---
+++ native/vdp.cpp
@@ -0,0 +1,13 @@
+#include "vdp.h"
+
+void vdp_init() {
+    // Inicialização do Video Display Processor
+}
+
+void vdp_reset() {
+    // Reset do VDP
+}
+
+void vdp_render_scanline(int line) {
+    // Renderizar uma linha de varredura
+}
