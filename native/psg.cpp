d6e7bb90e66f7f64db07e2a434cb8c5606973296
Rafael Carneiro <rafaelmdcarneiro@gmail.com>
2026-02-18 01:14:06 -0300

    Create psg.cpp
    Initial commit.

---
+++ native/psg.cpp
@@ -0,0 +1,13 @@
+#include "psg.h"
+
+void psg_init() {
+    // Inicialização do Programmable Sound Generator
+}
+
+void psg_reset() {
+    // Reset do PSG
+}
+
+void psg_execute(int cycles) {
+    // Executar geração de som por 'cycles' ciclos
+}
